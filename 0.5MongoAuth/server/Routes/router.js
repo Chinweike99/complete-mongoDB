547e7f90804af5784c20fd355a0c05045866c986
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-20 15:04:26 +0100

    Registeration

--- 0.5MongoAuth/server/Routes/router.js
+++ 0.5MongoAuth/server/Routes/router.js
@@ -2,18 +2,24 @@
   */ 
  import express from 'express';
  const router = express.Router();
+ import bcrypt from 'bcrypt'
  import signSchemaModel from '../Models/model.js';
 
  const app = express();
  app.use(express.json());
 
- router.post('/signup', (req, res) => {
+ router.post('/signup', async (req, res) => {
   console.log(req.body);
+
+  const salt = await bcrypt.genSaltSync(10);
+  const hashedPassword = await bcrypt.hashSync(req.body.password, salt)
+
+
     const signupUser = new signSchemaModel({
-        // fullName: req.body.fullName,
+        fullName: req.body.fullName,
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     })
     signupUser.save().then(result => res.json(result))
     .catch(error => res.json(error))
